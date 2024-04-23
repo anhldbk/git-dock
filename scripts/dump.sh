38102c62d3bf5dcdf70fb23bbf8261a7e4cbd8fc
Andy Le <anhldbk@gmail.com>
2024-04-23 22:07:15 +0700

    scripts: refactor

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
-docker save -o ollama-v0.1.32.tar ollama/ollama:0.1.32
+image="$1"
+
+file_path=registry/$(echo "$image" | sed 's/:/-/g').tar
+parent_dir=$(dirname "$file_path")
+
+echo -e "💾 Dumping $image...\n"
+echo "output path: $file_path"
+echo "parent dir: $parent_dir"
+
+mkdir -p "$parent_dir"
+docker save -o "$file_path" "$image"
+
+# Set the chunk size in bytes (80MB)
+chunk_size=$((80 * 1024 * 1024))
+
+# Split the file into chunks using split command
+split -b "$chunk_size" "$file_path" "$file_path"_
+
+echo -e "Image file is split successfully"
+rm -f "$file_path"
+echo "The file is removed. Please commit"
+
+echo -e "\n🚀 Done"
--- scripts/load.sh
+++ scripts/load.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-docker load -i ollama/ollama-v0.1.32.tar
+image="$1"
+
+file_path=registry/$(echo "$image" | sed 's/:/-/g').tar
+echo -e "💾 Loading $image...\n"
+echo -e "image path: $file_path"
+
+echo "Joining image files..."
+cat "$file_path"_* > "$file_path"
+
+docker load -i "$file_path"
+
+rm -f "$file_path"
+echo -e "The temporary image file is removed.\n"
+
+echo "🚀 Done"
